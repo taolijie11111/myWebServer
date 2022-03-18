4bcfe87edf1d78334e35ce9d028b7e4658a1b293
taolijie <taolijie11111@gmail.com>
2022-03-19 03:40:01 +0800

    add

--- log/log.cpp
+++ log/log.cpp
@@ -1,58 +1,75 @@
-#include "log.h"
-
-
-#include <sys/time.h>//gettimeofday
 #include <string.h>
+#include <time.h>
+#include <sys/time.h>
 #include <stdarg.h>
-/**
- *  #include <string.h>
+#include "log.h"
+#include <pthread.h>
+using namespace std;
 
-       void *memset(void *s, int c, size_t n);
+Log::Log()
+{
+    m_count = 0;
+    m_is_async = false;
+}
 
- * 
- */
-Log::Log(){
-m_count=0;
-m_is_async=false;
-}
-Log::~Log(){
-if(m_fp!=NULL)fclose(m_fp);
-}
-bool Log::init(const char *file_name, int close_log, int log_buf_size, int split_lines, int max_queue_size){
-    if(max_queue_size>=1){
-        m_is_async=true;
-        m_log_queue=new block_queue<std::string>(max_queue_size);
-        pthread_t tid;
-        pthread_create(&tid,NULL,flush_log_thread,NULL);
+Log::~Log()
+{
+    if (m_fp != NULL)
+    {
+        fclose(m_fp);
     }
-
-    m_close_log=close_log;
-    m_long_buf_size=log_buf_size;
-    m_buf=new char[m_long_buf_size];
-    memset(m_buf,'\0',m_long_buf_size);//memset 表示复制字符 c（一个无符号字符）到参数 str 所指向的字符串的前 n 个字符。返回的是m_long_buf_size
+}
+//异步需要设置阻塞队列的长度，同步不需要设置
+bool Log::init(const char *file_name, int close_log, int log_buf_size, int split_lines, int max_queue_size)
+{
+    //如果设置了max_queue_size,则设置为异步
+    if (max_queue_size >= 1)
+    {
+        m_is_async = true;
+        m_log_queue = new block_queue<string>(max_queue_size);
+        pthread_t tid;
+        //flush_log_thread为回调函数,这里表示创建线程异步写日志
+        pthread_create(&tid, NULL, flush_log_thread, NULL);
+    }
     
+    m_close_log = close_log;
+    m_log_buf_size = log_buf_size;
+    m_buf = new char[m_log_buf_size];
+    memset(m_buf, '\0', m_log_buf_size);
+    m_split_lines = split_lines;
+
     time_t t = time(NULL);
-    struct tm *sys_tm=localtime(&t);
-    struct tm my_tm=*sys_tm;
+    struct tm *sys_tm = localtime(&t);
+    struct tm my_tm = *sys_tm;
 
-    const char *p=strrchr(file_name,'/');
-    char log_full_name[256] ={0};
+ 
+    const char *p = strrchr(file_name, '/');
+    char log_full_name[256] = {0};
 
-    if(p==NULL){
+    if (p == NULL)
+    {
         snprintf(log_full_name, 255, "%d_%02d_%02d_%s", my_tm.tm_year + 1900, my_tm.tm_mon + 1, my_tm.tm_mday, file_name);
-        //C 库函数 char *strrchr(const char *str, int c) 在参数 str 所指向的字符串中搜索最后一次出现字符 c（一个无符号字符）的位置
-    }else{
+    }
+    else
+    {
         strcpy(log_name, p + 1);
         strncpy(dir_name, file_name, p - file_name + 1);
         snprintf(log_full_name, 255, "%s%d_%02d_%02d_%s", dir_name, my_tm.tm_year + 1900, my_tm.tm_mon + 1, my_tm.tm_mday, log_name);
     }
-    m_today=my_tm.tm_mday;
-    m_fp=fopen(log_full_name,"a");
-    if(m_fp==NULL)return false;
+
+    m_today = my_tm.tm_mday;
+    
+    m_fp = fopen(log_full_name, "a");
+    if (m_fp == NULL)
+    {
+        return false;
+    }
+
     return true;
 }
 
-void Log::write_log(int level, const char *format, ...){        
+void Log::write_log(int level, const char *format, ...)
+{
     struct timeval now = {0, 0};
     gettimeofday(&now, NULL);
     time_t t = now.tv_sec;
@@ -109,7 +126,7 @@ void Log::write_log(int level, const char *format, ...){
     va_list valst;
     va_start(valst, format);
 
-    std::string log_str;
+    string log_str;
     m_mutex.lock();
 
     //写入的具体时间内容格式
@@ -117,7 +134,7 @@ void Log::write_log(int level, const char *format, ...){
                      my_tm.tm_year + 1900, my_tm.tm_mon + 1, my_tm.tm_mday,
                      my_tm.tm_hour, my_tm.tm_min, my_tm.tm_sec, now.tv_usec, s);
     
-    int m = vsnprintf(m_buf + n, m_long_buf_size - 1, format, valst);
+    int m = vsnprintf(m_buf + n, m_log_buf_size - 1, format, valst);
     m_buf[n + m] = '\n';
     m_buf[n + m + 1] = '\0';
     log_str = m_buf;
@@ -133,13 +150,15 @@ void Log::write_log(int level, const char *format, ...){
         m_mutex.lock();
         fputs(log_str.c_str(), m_fp);
         m_mutex.unlock();
-    }
+            }
 
     va_end(valst);
 }
 
-void Log::flush(void){
+void Log::flush(void)
+{
     m_mutex.lock();
+    //强制刷新写入流缓冲区
     fflush(m_fp);
     m_mutex.unlock();
 }
