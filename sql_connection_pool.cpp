12c5891619056a96ea227df8cf1877429410fa6a
taolijie <taolijie11111@gmail.com>
2022-03-22 15:19:06 +0800

    add

--- sql_connection_pool.cpp
+++ sql_connection_pool.cpp
@@ -22,43 +22,6 @@ connection_pool *connection_pool::GetInstance()
 	return &connPool;
 }
 
-//构造初始化
-// void connection_pool::init(string url, string User, string PassWord, string DBName, int Port, int MaxConn, int close_log)
-// {
-// 	m_url = url;
-// 	m_Port = Port;
-// 	m_User = User;
-// 	m_PassWord = PassWord;
-// 	m_DatabaseName = DBName;
-// 	m_close_log = close_log;
-
-// 	for (int i = 0; i < MaxConn; i++)
-// 	{
-// 		MYSQL *con = NULL;
-// 		con = mysql_init(con);
-
-// 		if (con == NULL)
-// 		{
-// 			LOG_ERROR("MySQL Error,sql_connetion wrong 42");
-// 			exit(1);
-// 		}
-// 		con = mysql_real_connect(con, url.c_str(), User.c_str(), PassWord.c_str(), DBName.c_str(),9006, NULL, 0);
-
-// 		if (con == NULL)
-// 		{
-// 			mysql_error(con);
-// 			LOG_ERROR("MySQL ERROR");
-// 			exit(1);
-// 		}
-// 		connList.push_back(con);
-// 		++m_FreeConn;
-// 	}
-
-// 	reserve = sem(m_FreeConn);
-
-// 	m_MaxConn = m_FreeConn;
-// }
-
 //有打印错误信息版本
 
 void connection_pool::init(string url, string User, string PassWord, string DBName, int Port, int MaxConn, int close_log)
